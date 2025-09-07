6977219eaee6371a6c0f23528b87018773b096d1
juan-dot-hub <Sseunghwan7@gmail.com>
2025-09-07 20:02:20 +0900

    Update data fitting 2.py

--- data fitting 2.py
+++ data fitting 2.py
@@ -106,7 +106,7 @@ if st.button("피팅 실행"):
     results2 = fit_and_plot(t_data2, vr_observed2, "data set 2")
     
     # 그래프 그리기
-    fig, axs = plt.subplots(1, 1, figsize=(10, 8), gridspec_kw={'height_ratios': [3, 1]})
+    fig, axs = plt.subplots(1, 1, figsize=(10, 8))
     axs[0].errorbar(results1[0], results1[1], yerr=1.5, fmt='ro', label=f'{results1[4]} observed')
     axs[0].plot(results1[0], results1[2], 'r-', label=f'{results1[4]} fitted curve')
     axs[0].errorbar(results2[0], results2[1], yerr=1.5, fmt='bo', label=f'{results2[4]} observed')
